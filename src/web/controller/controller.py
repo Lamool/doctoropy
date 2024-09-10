950656e82aa2a6aa8273c9482d9c0652fcb89814
kimruafase <ppooppplus33@gmail.com>
2024-09-10 17:50:33 +0900

    240910 김민석 17:50

--- src/web/controller/controller.py
+++ src/web/controller/controller.py
@@ -1,8 +1,15 @@
 from src.web.app import app
 
+from src.web.service.service import *
+from src.web.app import *
 # 김민석 112233
 #
 
-@app.route("/", methods = ['GET'])
-def index() :
-    return [1,2,3]
+@app.route("/rate/cal", methods = ['GET'])
+def rate_cal() :
+    n1 = request.args["n1"]
+    m1 = request.args["m1"]
+    n2 = request.args["n2"]
+    m2 = request.args["m2"]
+    rate_dict = poke(int(n1), int(m1), int(n2), int(m2))
+    return rate_dict
--- src/web/model/rate_class.py
+++ src/web/model/rate_class.py
@@ -2,23 +2,25 @@
 
 
 class rate_poke_mon_A :
-    def __init__(self, atk, _def, spe_atk, spe_def, speed, type, sk_dam):
+    def __init__(self, hp, atk, _def, spe_atk, spe_def, speed, type, sk_dam):
+        self.hp = int(hp)
         self.atk = int(atk)
         self._def = int(_def)
         self.spe_atk = int(spe_atk)
         self.spe_def = int(spe_def)
         self.speed = int(speed)
         self.sk_dam = int(sk_dam)
-        self.type = int(type)
+        self.type = type
 
 
 
 class rate_poke_mon_B :
-    def __init__(self, atk, _def, spe_atk, spe_def, speed, type, sk_dam):
+    def __init__(self, hp, atk, _def, spe_atk, spe_def, speed, type, sk_dam):
+        self.hp = int(hp)
         self.atk = int(atk)
         self._def = int(_def)
         self.spe_atk = int(spe_atk)
         self.spe_def = int(spe_def)
         self.speed = int(speed)
         self.sk_dam = int(sk_dam)
-        self.type = int(type)
+        self.type = type
