7b60afd14f42dbdf9d976431c18b2676b2eae394
kimruafase <ppooppplus33@gmail.com>
2024-09-11 17:54:50 +0900

    240911 김민석 17:54

--- src/web/controller/controller.py
+++ src/web/controller/controller.py
@@ -1,3 +1,5 @@
+import json
+
 from src.web.app import app
 
 from src.web.service.service import *
@@ -13,3 +15,26 @@ def rate_cal() :
     m2 = request.args["m2"]
     rate_dict = poke(int(n1), int(m1), int(n2), int(m2))
     return rate_dict
+
+
+@app.route("/rate/data_info", methods = ["GET"])
+def rate_data_info() :
+    n = request.args["n"]
+    poke_data_info = poke_rate_data_info(int(n))
+    return poke_data_info
+
+@app.route("/rate/skill_info", methods = ["GET"])
+def rate_skill_info() :
+    n = request.args["n"]
+    poke_data_info = poke_rate_skill_info(int(n))
+    return poke_data_info
+
+@app.route("/rate/all_info", methods=["GET"])
+def rate_data_all() :
+    poke_data_json_loads = poke_info_num()
+    return poke_data_json_loads
+
+@app.route("/rate/all_skill", methods=["GET"])
+def rate_skill_all() :
+    poke_data_json_loads = poke_skill_num()
+    return poke_data_json_loads
