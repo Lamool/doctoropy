44f67bdd3aadfd2b9036992006013ed93a0df8cd
Kimviruscool <sinsa9122@naver.com>
2024-09-13 18:04:04 +0900

    24 09 13

--- src/web/service/rankservice.py
+++ src/web/service/rankservice.py
@@ -34,14 +34,10 @@ def index():
 def tournament(set_size) :
     df = pd.read_csv("./service/datapokemon.csv", encoding="utf-8")
     df3 = df[["한글이름","이미지","아이디"]]  # 토너먼트 이름용
-    df3_list = df3.values.tolist()  # 이차원리스트 일차원리스트로 변경
+    df3_list = df3.values.tolist()  # random위해 2차원 리스트로 변경
     select_pokemon = random.sample(df3_list, set_size)
-    # print(type(select_pokemon))
-    select_pokemon2 = [select_pokemon]
-    select_pokemon3 = pd.DataFrame(select_pokemon2)
+    # print(select_pokemon)
+    select_pokemon3 = pd.DataFrame(select_pokemon)
     jsondata = select_pokemon3.to_json(orient='records', force_ascii=False)
-    print(jsondata)
-    print(json.loads(jsondata))
     result = json.loads(jsondata)
     return jsondata
-    # print(select_pokemon)
