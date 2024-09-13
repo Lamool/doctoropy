def17545edd008aba9eb50d48c0a62fdc62a0c93
kimruafase <ppooppplus33@gmail.com>
2024-09-14 03:17:53 +0900

    240914 03:17 김민석 커밋

--- src/web/controller/info_controller.py
+++ src/web/controller/info_controller.py
@@ -16,3 +16,10 @@ def poke_info_detail_print() :
     name = request.args["name"]
     df_poke_detail_info_result = poke_detail_info_print(name)
     return df_poke_detail_info_result
+
+@app.route("/info/type", methods=["GET"])
+def poke_info_type_print():
+    type = request.args["type"]
+    page = request.args["page"]
+    df_poke_type_list_all_result = type_poke_info(type, int(page))
+    return df_poke_type_list_all_result
