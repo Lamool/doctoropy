21a4f1fc97d961f456a867fa9a3dc3f81e0c482d
kimruafase <ppooppplus33@gmail.com>
2024-09-13 17:56:53 +0900

    240913 김민석 커밋 17:56

--- src/web/controller/info_controller.py
+++ src/web/controller/info_controller.py
@@ -1,1 +1,18 @@
 # info_controller.py
+
+
+from src.web.app import *
+from src.web.service.info_service import *
+
+@app.route("/info/print", methods=["GET"])
+def poke_info_all_print() :
+    page = request.args["page"]
+    print(page)
+    df_list_all_result = poke_all_info_print(int(page))
+    return df_list_all_result
+
+@app.route("/info/detail", methods=["GET"])
+def poke_info_detail_print() :
+    name = request.args["name"]
+    df_poke_detail_info_result = poke_detail_info_print(name)
+    return df_poke_detail_info_result
--- src/web/service/info_service.py
+++ src/web/service/info_service.py
@@ -3,10 +3,63 @@ import json
 
 import pandas as pd
 
-poke_data = pd.read_csv("datapokemon.csv", encoding="utf-8", index_col=0)
+
 
 # print(poke_data)
 #
-# print(poke_data.loc[[0:20], ["영어이름"], ["이미지"]])
+# print(poke_data.loc[])
 # print(poke_data.loc[0:20,"영어이름"])
 # print(poke_data.loc[0:20,"이미지"])
+
+
+def poke_all_info_print(page) :
+    list = []
+    list_all = []
+
+    poke_data = pd.read_csv("./service/datapokemon.csv", encoding="utf-8", index_col=0)
+    n = 0
+    m = 100
+    if page >= 1000 :
+        for i1 in range(1000, 1025):
+            i_poke_data = poke_data.iloc[i1]
+            i_poke_data_dict = {"이미지": i_poke_data["이미지"], "한글이름": i_poke_data["한글이름"], "영어이름": i_poke_data["영어이름"]}
+            list.append(i_poke_data_dict)
+            # print(f"시작 번호 : {n}, 끝 번호 : {m}")
+            # print(i_poke_data_dict)
+        list_all = [list]
+        # print(list)
+        # print(list_all)
+        df_list_all = pd.DataFrame(list_all)
+        df_list_all_json = df_list_all.to_json(orient="records", force_ascii=False)
+        df_list_all_result = json.loads(df_list_all_json)
+        return df_list_all_result
+    else :
+        for i1 in range(n + page , m + page) :  #  0+0  , 100+0  , 0+100 , 100+100
+            i_poke_data = poke_data.iloc[i1]
+            i_poke_data_dict = {"이미지" : i_poke_data["이미지"], "한글이름" : i_poke_data["한글이름"], "영어이름" : i_poke_data["영어이름"]}
+            list.append(i_poke_data_dict)
+            # print(f"시작 번호 : {n}, 끝 번호 : {m}")
+            # print(i_poke_data_dict)
+        list_all = [list]
+        # print(list)
+        # print(list_all)
+        df_list_all = pd.DataFrame(list_all)
+        df_list_all_json = df_list_all.to_json(orient="records", force_ascii=False)
+        df_list_all_result = json.loads(df_list_all_json)
+        return df_list_all_result
+
+
+def poke_detail_info_print(name) :
+    poke_data = pd.read_csv("./service/datapokemon.csv", encoding="utf-8", index_col=0)
+    name_poke_data = poke_data.loc[:, "영어이름"]
+
+    for i, n in enumerate(name_poke_data) :
+        if n == name :
+            poke_detail_info_dict = [poke_data.iloc[i]]
+            print(poke_detail_info_dict)
+            df_poke_detail_info_list = pd.DataFrame(poke_detail_info_dict)
+            df_poke_detail_info_json = df_poke_detail_info_list.to_json(orient="records", force_ascii=False)
+            df_poke_detail_info_result = json.loads(df_poke_detail_info_json)
+            print(df_poke_detail_info_result)
+            return df_poke_detail_info_result
+
