6a411c680010370d4e527c1262ad153dbbe13eb3
kimruafase <ppooppplus33@gmail.com>
2024-09-15 19:07:52 +0900

    240915 19:07 김민석 커밋

--- src/web/service/info_service.py
+++ src/web/service/info_service.py
@@ -22,7 +22,9 @@ def poke_all_info_print(page) :
     if page >= 1000 :
         for i1 in range(1000, 1025):
             i_poke_data = poke_data.iloc[i1]
-            i_poke_data_dict = {"이미지": i_poke_data["이미지"], "한글이름": i_poke_data["한글이름"], "영어이름": i_poke_data["영어이름"]}
+            i_poke_data_dict = {"이미지": i_poke_data["이미지"],
+                                "한글이름": i_poke_data["한글이름"],
+                                "영어이름": i_poke_data["영어이름"]}
             list.append(i_poke_data_dict)
             # print(f"시작 번호 : {n}, 끝 번호 : {m}")
             # print(i_poke_data_dict)
@@ -36,7 +38,9 @@ def poke_all_info_print(page) :
     else :
         for i1 in range(n + page , m + page) :  #  0+0  , 100+0  , 0+100 , 100+100
             i_poke_data = poke_data.iloc[i1]
-            i_poke_data_dict = {"이미지" : i_poke_data["이미지"], "한글이름" : i_poke_data["한글이름"], "영어이름" : i_poke_data["영어이름"]}
+            i_poke_data_dict = {"이미지" : i_poke_data["이미지"],
+                                "한글이름" : i_poke_data["한글이름"],
+                                "영어이름" : i_poke_data["영어이름"]}
             list.append(i_poke_data_dict)
             # print(f"시작 번호 : {n}, 끝 번호 : {m}")
             # print(i_poke_data_dict)
@@ -198,11 +202,15 @@ def poke_kr_search(input_kr_name, page_number):
         if len(input_kr_name) == 1:
             # 길이가 1인 경우, 이름에 포함된 모든 경우를 반환
             if input_kr_name in kr_name:
-                results.append({"한글이름": kr_name, "이미지": poke_data_images.iloc[i], "영어이름": poke_data_en_name.iloc[i]})
+                results.append({"한글이름": kr_name,
+                                "이미지": poke_data_images.iloc[i],
+                                "영어이름": poke_data_en_name.iloc[i]})
         else:
             # 길이가 2 이상인 경우
             if input_kr_name in kr_name:
-                results.append({"한글이름": kr_name, "이미지": poke_data_images.iloc[i], "영어이름": poke_data_en_name.iloc[i]})
+                results.append({"한글이름": kr_name,
+                                "이미지": poke_data_images.iloc[i],
+                                "영어이름": poke_data_en_name.iloc[i]})
 
     # 페이지 범위 계산
     start_index = (page_number - 1) * items_per_page
@@ -237,11 +245,15 @@ def poke_en_search(input_en_name, page_number):
         if len(input_en_name) == 1:
             # 길이가 1인 경우, 이름에 포함된 모든 경우를 반환
             if input_en_name in en_name:
-                results.append({"영어이름": en_name, "이미지": poke_data_images.iloc[i], "한글이름": poke_data_kr_name.iloc[i]})
+                results.append({"영어이름": en_name,
+                                "이미지": poke_data_images.iloc[i],
+                                "한글이름": poke_data_kr_name.iloc[i]})
         else:
             # 길이가 2 이상인 경우
             if input_en_name in en_name:
-                results.append({"영어이름": en_name, "이미지": poke_data_images.iloc[i], "한글이름": poke_data_kr_name.iloc[i]})
+                results.append({"영어이름": en_name,
+                                "이미지": poke_data_images.iloc[i],
+                                "한글이름": poke_data_kr_name.iloc[i]})
 
     # 페이지 범위 계산
     start_index = (page_number - 1) * items_per_page
@@ -279,13 +291,11 @@ def poke_type_kr_search(input_kr_name, type_eng, page_number):
     poke_data_kr_name = poke_data['한글이름']
 
     find_type_kr_name = []
-    find_type_kr_index = []
 
     for i in index_list :
         # print(i)
         # print(poke_data_kr_name.iloc[i])
         find_type_kr_name.append(poke_data_kr_name.iloc[i])
-        find_type_kr_index.append(i)
     # print(find_type_kr_name)
 
     poke_data_images = poke_data['이미지']
@@ -301,14 +311,14 @@ def poke_type_kr_search(input_kr_name, type_eng, page_number):
             # 길이가 1인 경우, 이름에 포함된 모든 경우를 반환
             if input_kr_name in kr_name:
                 results.append({"한글이름": kr_name,
-                                "이미지": poke_data_images.iloc[find_type_kr_index[i]],
-                                "영어이름": poke_data_en_name.iloc[find_type_kr_index[i]]})
+                                "이미지": poke_data_images.iloc[index_list[i]],
+                                "영어이름": poke_data_en_name.iloc[index_list[i]]})
         else:
             # 길이가 2 이상인 경우
             if input_kr_name in kr_name:
                 results.append({"한글이름": kr_name,
-                                "이미지": poke_data_images.iloc[find_type_kr_index[i]],
-                                "영어이름": poke_data_en_name.iloc[find_type_kr_index[i]]})
+                                "이미지": poke_data_images.iloc[index_list[i]],
+                                "영어이름": poke_data_en_name.iloc[index_list[i]]})
 
     # print(results)
     start_index = (page_number - 1) * items_per_page
@@ -335,13 +345,11 @@ def poke_type_en_search(input_en_name, type_eng, page_number):
     poke_data_en_name = poke_data["영어이름"]
 
     find_type_en_name = []
-    find_type_en_index = []
 
     for i in index_list :
         # print(i)
         # print(poke_data_kr_name.iloc[i])
         find_type_en_name.append(poke_data_en_name.iloc[i])
-        find_type_en_index.append(i)
     # print(find_type_kr_name)
 
     poke_data_images = poke_data['이미지']
@@ -357,14 +365,14 @@ def poke_type_en_search(input_en_name, type_eng, page_number):
             # 길이가 1인 경우, 이름에 포함된 모든 경우를 반환
             if input_en_name in en_name:
                 results.append({"영어이름": en_name,
-                                "이미지": poke_data_images.iloc[find_type_en_index[i]],
-                                "한글이름": poke_data_kr_name.iloc[find_type_en_index[i]]})
+                                "이미지": poke_data_images.iloc[index_list[i]],
+                                "한글이름": poke_data_kr_name.iloc[index_list[i]]})
         else:
             # 길이가 2 이상인 경우
             if input_en_name in en_name:
                 results.append({"영어이름": en_name,
-                                "이미지": poke_data_images.iloc[find_type_en_index[i]],
-                                "한글이름": poke_data_kr_name.iloc[find_type_en_index[i]]})
+                                "이미지": poke_data_images.iloc[index_list[i]],
+                                "한글이름": poke_data_kr_name.iloc[index_list[i]]})
 
     # print(results)
     start_index = (page_number - 1) * items_per_page
