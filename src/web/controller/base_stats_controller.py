342d2dd879740b52c332e2099206e9a3744eb83f
Lamool <gosu0522@naver.com>
2024-09-19 18:12:23 +0900

    240919 18:12 종족값 목록 총합 빼고 내림차순 가능

--- src/web/controller/base_stats_controller.py
+++ src/web/controller/base_stats_controller.py
@@ -2,8 +2,41 @@ from src.web.app import *
 from src.web.service.base_stats_service import *
 
 # 포켓몬 종족값 목록 -> 아이디(번호)를 기준으로 출력 [오름차순]
-@app.route("/base/stats/print", methods=["GET"])
+# @app.route("/base/stats/print", methods=["POST"])
+# def base_stats_all_print() :
+#     data = request.json  # JSON 형식으로 데이터 받기
+#     print(data)  # 서버 콘솔에 출력
+#     if data['data'] == 101010 :
+#         print("aaaaa")
+#     else :
+#         print("bbbbb")
+#     result = base_stats_print_all()
+#     print(result)
+#     return result
+#
+# # 포켓몬 종족값 목록 -> 아이디(번호)를 기준으로 출력 [내림차순]
+# @app.route("/base/stats/print/pokeno", methods=["GET"])
+# def base_stats_pokeno_print() :
+#     result = base_stats_print_pokeno()
+#     # print(result)
+#     return result
+
+
+
+
+
+
+
+
+# 포켓몬 종족값 목록 -> 아이디(번호)를 기준으로 출력 [오름차순]
+@app.route("/base/stats/print", methods=["POST"])
 def base_stats_all_print() :
+    # data = request.json  # JSON 형식으로 데이터 받기
+    # print(data)  # 서버 콘솔에 출력
+    # if data['data'] == 101010 :
+    #     print("aaaaa")
+    # else :
+    #     print("bbbbb")
     result = base_stats_print_all()
     print(result)
     return result
@@ -14,3 +47,17 @@ def base_stats_pokeno_print() :
     result = base_stats_print_pokeno()
     # print(result)
     return result
+
+
+# 포켓몬 종족값 목록 -> 포켓몬명을 기준으로 출력
+@app.route("/base/stats/print/pokename", methods=["POST"])
+def base_stats_pokename_print() :
+    data = request.json  # JSON 형식으로 데이터 받기
+    print(data)  # 서버 콘솔에 출력
+
+    result = base_stats_print_pokename(data)
+    # print(result)
+    return result
+
+
+
