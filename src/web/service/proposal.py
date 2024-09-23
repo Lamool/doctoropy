588203968aa5230d99dea3e59d8084983ae6a5b3
Kimviruscool <sinsa9122@naver.com>
2024-09-23 18:11:57 +0900

    24 09 23 머신러닝 모델 생성중

--- src/web/service/proposal.py
+++ src/web/service/proposal.py
@@ -3,10 +3,10 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression #모델 객체 생성
 from sklearn.model_selection import train_test_split # 모델 훈련용,테스트용 분류
 from sklearn.metrics import mean_squared_error,r2_score #MSE, R² 값 분석
-
+'''
 #X ,Y 분할
-# Y = data_df[''] #타겟
-# X = data_df[''] #피처
+Y = data_df[''] #타겟
+X = data_df[''] #피처
 
 X_train , X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
 #비율 7:3 설정
@@ -28,11 +28,9 @@ print(r2)
 print("Y 절편 값 :", np.round(lr.intercept_, 2))
 print("회귀 계수 값 :", np.round(lr.coef_, 2))
 
-# coef = pd.Series(data = np.round(lr.coef_,2), index=X.columns) #Series 자료형 생성
-# coef.sort_values(ascending=False) #내림 차순
-
-# 생성중 접근 금지
+coef = pd.Series(data = np.round(lr.coef_,2), index=X.columns) #Series 자료형 생성
+coef.sort_values(ascending=False) #내림 차순
+'''
 
 # @app.route("/pro")
 # def proposal():
-#
