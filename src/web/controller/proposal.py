a87c18a348924ec05b028668f39c54bcde62b654
Kimviruscool <sinsa9122@naver.com>
2024-09-27 01:10:03 +0900

    2024-09-27

--- src/web/controller/proposal.py
+++ src/web/controller/proposal.py
@@ -5,21 +5,27 @@ from src.web.app import *
 from flask import request
 
 from src.web.service.proposal import *
-
+from flask import jsonify
+from src.web.app import *
 
 #가져오기
-@app.route('/model/get', methods=["GET"])
+@app.route('/model', methods=["POST"])
 def fetch_data() :
     java_url = 'http://localhost:8080/pro/get'
-    uno_url = request.args.get('user')
-    print(uno_url)
-    response = requests.get(java_url) #JAVA controller api 호출
-    # response2 = requests.get(uno_url)
-    # print(response2)
-    data = response.json() #json 데이터형식으로 파싱
-    # users = response2.json()
-    # print(users)
-    result = modeling(data, uno_url)
 
+    user = request.get_json()
+
+    gender = user.get('gender')
+    ubirth = user.get('ubirth')
+
+    response = requests.get(java_url) #JAVA controller api 호출
+    data = response.json() #json 데이터형식으로 파싱
+    # print(data)
+    result = modeling(data, gender, ubirth)
+    post_data(result)
+    return result
+
+@app.route('/promodel', methods=["GET"])
+def post_data(result) :
     print(result)
     return result
