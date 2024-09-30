78e9f9a4207d74446500f92ac5b1fd525167c4cf
Kimviruscool <sinsa9122@naver.com>
2024-09-30 15:46:17 +0900

    24 09 30 머신러닝 완성

--- src/web/controller/proposal.py
+++ src/web/controller/proposal.py
@@ -14,28 +14,37 @@ from src.web.app import *
 @app.route('/model', methods=["GET"])
 def fetch_data() :
     java_url = 'http://localhost:8080/pro/get'
+    # uno_url = 'http://localhost:8080/my/info'
+
+    gender = request.args["gender"]
+    ubirth = request.args["ubirth"]
+
+    print(ubirth)
+    print(gender)
 
     # print(java_url)
-    # print(uno_url)
 
     response = requests.get(java_url) #JAVA controller api 호출
+    # response2 = requests.get(uno_url)
     # print(response)
 
     data = response.json() #json 데이터형식으로 파싱
 
-    result = modeling(data)
+    result = modeling(data,int(ubirth),int(gender))
     print(result)
 
     return str(result)
 
-@app.route('/ubirth', method=["GET"])
-def unoubirth() :
-    ubirth = request.args["ubirth"]
-    result = modeling(int(ubirth))
-    return json.loads(result)
-
-@app.route('/gender', method=["GET"])
-def gender() :
-    gender = request.args["gender"]
-    result = modeling(int(gender))
-    return json.loads(result)
+# @app.route('/ubirth', methods=["GET"])
+# def unoubirth() :
+#     ubirth = request.args["ubirth"]
+#     # print(ubirth)
+#     result = modeling(int(ubirth))
+#     return json.loads(result)
+#
+# @app.route('/gender', methods=["GET"])
+# def gender() :
+#     gender = request.args["gender"]
+#     # print(gender)
+#     result = modeling(int(gender))
+#     return json.loads(result)
