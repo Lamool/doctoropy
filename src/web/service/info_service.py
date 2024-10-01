557935703c37e6b2c5b6c92535d88efd562bcccd
kimruafase <ppooppplus33@gmail.com>
2024-10-01 18:09:51 +0900

    241001 김민석 커밋 18:10

--- src/web/service/info_service.py
+++ src/web/service/info_service.py
@@ -14,7 +14,6 @@ import pandas as pd
 
 def poke_all_info_print(page) :
     list = []
-    list_all = []
 
     poke_data = pd.read_csv("./service/datapokemon.csv", encoding="utf-8", index_col=0)
     n = 0
@@ -26,27 +25,23 @@ def poke_all_info_print(page) :
                                 "한글이름": i_poke_data["한글이름"],
                                 "영어이름": i_poke_data["영어이름"]}
             list.append(i_poke_data_dict)
-            # print(f"시작 번호 : {n}, 끝 번호 : {m}")
-            # print(i_poke_data_dict)
+
         list_all = [list]
-        # print(list)
-        # print(list_all)
+
         df_list_all = pd.DataFrame(list_all)
         df_list_all_json = df_list_all.to_json(orient="records", force_ascii=False)
         df_list_all_result = json.loads(df_list_all_json)
         return df_list_all_result
     else :
-        for i in range(n + page , m + page) :  #  0+0  , 100+0  , 0+100 , 100+100
+        for i in range(n + page , m + page):
             i_poke_data = poke_data.iloc[i]
             i_poke_data_dict = {"이미지" : i_poke_data["이미지"],
                                 "한글이름" : i_poke_data["한글이름"],
                                 "영어이름" : i_poke_data["영어이름"]}
             list.append(i_poke_data_dict)
-            # print(f"시작 번호 : {n}, 끝 번호 : {m}")
-            # print(i_poke_data_dict)
+
         list_all = [list]
-        # print(list)
-        # print(list_all)
+
         df_list_all = pd.DataFrame(list_all)
         df_list_all_json = df_list_all.to_json(orient="records", force_ascii=False)
         df_list_all_result = json.loads(df_list_all_json)
@@ -96,36 +91,32 @@ def poke_detail_info_print(name) :
 # page_number = int(input("페이지 번호 입력: "))  # 예를 들어 1페이지, 2페이지 등
 
 def get_paged_data(index_list, page_size, page_number):
-    # print(index_list)
-    # print(page_size)
-    # print(page_number)
+
     start_index = page_size * (page_number - 1)
     end_index = start_index + page_size
 
-# 범위를 초과하지 않도록 end_index 조정
+    # 범위를 초과하지 않도록 end_index 조정
     if end_index > len(index_list):
         end_index = len(index_list)
     return index_list[start_index:end_index]
+
 def type_poke_info(type_eng, page_number) :
     # 사용자 입력 받기
     # 데이터 읽기
-    # print(page_number)
-    # print(type_eng)
     poke_data = pd.read_csv("./service/datapokemon.csv", encoding="utf-8", index_col=0)
-    # print(poke_data)
+
     # 타입 정보 추출
     poke_type = poke_data.loc[:, "타입"]
     index_list = [i for i, t in enumerate(poke_type) if t == type_eng]
-    # print(index_list)
+
     # 페이지 크기와 페이지 번호 설정
     page_size = 100
-    # print(index_list)
+
 
     poke_data['한글이름'] = poke_data['한글이름'].fillna('').astype(str)
     # 페이지에 해당하는 인덱스 리스트 추출
     paged_index_list = get_paged_data(index_list, page_size, page_number)
-    # print(paged_index_list)
-    ##
+
     # 페이지에 해당하는 데이터 처리
     poke_type_info_list = []
     for i in paged_index_list:
@@ -136,7 +127,7 @@ def type_poke_info(type_eng, page_number) :
             "영어이름": poke_data_index["영어이름"]
         }
         poke_type_info_list.append(poke_data_index_dict)
-    # print(poke_type_info_list)
+
     return poke_type_info_list
     # 결과 출력
     # for info in poke_type_info_list:
@@ -263,14 +254,12 @@ def poke_en_search(input_en_name, page_number):
 def poke_type_kr_info_search(type_eng, input_kr_name, page_number) :
     # 사용자 입력 받기
     # 데이터 읽기
-    # print(page_number)
-    # print(type_eng)
     poke_data = pd.read_csv("./service/datapokemon.csv", encoding="utf-8", index_col=0)
-    # print(poke_data)
+
     # 타입 정보 추출
     poke_type = poke_data.loc[:, "타입"]
     index_list = [i for i, t in enumerate(poke_type) if t == type_eng]
-    # print(index_list)
+
     # 페이지 크기와 페이지 번호 설정
     items_per_page = 100
 
@@ -302,7 +291,6 @@ def poke_type_kr_info_search(type_eng, input_kr_name, page_number) :
     start_index = (page_number - 1) * items_per_page
     end_index = start_index + items_per_page
     poke_search_results = results[start_index:end_index]
-    # print(poke_search_results)
     return poke_search_results
 
 
