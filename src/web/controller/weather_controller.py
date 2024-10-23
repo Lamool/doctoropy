559e00e01c79e60a9a04326ec53f9f60395b0c98
wonsebe <wonsb03129@gmail.com>
2024-10-23 18:15:41 +0900

    원세빈 1023 날씨 시간 받기 중 오류

--- src/web/controller/weather_controller.py
+++ src/web/controller/weather_controller.py
@@ -1,17 +1,19 @@
-#
-# from src.web.app import *
+
 # from flask import Flask, request, jsonify
 # from src.web.service.weather_service import *
-# @app.route('/updateTime',methods=['get'])
-# def update_time():
-#    java_url = 'http://localhost:8080/weather/'
-#    response = requests.get(java_url)  # JAVA controller api 호출
-#    print(response)
-#    year=request.args.get('year'),
-#    month=request.args.get('month'),
-#    day=request.args.get('day'),
-#    hours=request.args.get('hours'),
-#    minutes=request.args.get('minutes')
-#    print(f'현재 시간: {year}-{month}-{day} {hours}:{minutes}')
-#
-#    return jsonify({'status': 'success', 'message': '시간이 업데이트되었습니다.'})
+from src.web.app import *
+# CORS(app, supports_credentials=True, resources={r"*": {"origins": "*"}})  # 모든 출처 허용
+@app.route('/updateTime',methods=['get'])
+def update_time():
+   # java_url = 'http://localhost:8080/weather/'
+   # response = requests.get(java_url)  # JAVA controller api 호출
+   # print(response)
+
+   year=request.args.get('year'),
+   month=request.args.get('month'),
+   day=request.args.get('day'),
+   hours=request.args.get('hours'),
+   minutes=request.args.get('minutes')
+   print(f'현재 시간: {year}-{month}-{day} {hours}:{minutes}')
+
+   return jsonify({'status': 'success', 'message': '시간이 업데이트되었습니다.'})
