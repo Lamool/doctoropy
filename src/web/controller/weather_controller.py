1dedc4c1743dab23b647f2e9598c98912054fcd5
wonsebe <wonsb03129@gmail.com>
2024-10-24 18:21:30 +0900

    원세빈 1023 날씨 시간 받기 중 오류 고침

--- src/web/controller/weather_controller.py
+++ src/web/controller/weather_controller.py
@@ -1,19 +1,14 @@
-#
-# # from flask import Flask, request, jsonify
-# # from src.web.service.weather_service import *
-# from src.web.app import *
-# # CORS(app, supports_credentials=True, resources={r"*": {"origins": "*"}})  # 모든 출처 허용
-# @app.route('/updateTime',methods=['get'])
-# def update_time():
-#    # java_url = 'http://localhost:8080/weather/'
-#    # response = requests.get(java_url)  # JAVA controller api 호출
-#    # print(response)
-#
-#    year=request.args.get('year'),
-#    month=request.args.get('month'),
-#    day=request.args.get('day'),
-#    hours=request.args.get('hours'),
-#    minutes=request.args.get('minutes')
-#    print(f'현재 시간: {year}-{month}-{day} {hours}:{minutes}')
-#
-#    return jsonify({'status': 'success', 'message': '시간이 업데이트되었습니다.'})
+from flask import Flask, request, jsonify
+from src.web.service.weather_service import *
+from src.web.app import *
+@app.route('/updateTime',methods=['get'])
+def update_time():
+   year = int(request.args.get('year'))
+   month = int(request.args.get('month'))
+   date = int(request.args.get('date'))
+   hours = int(request.args.get('hours'))
+   minutes = int(request.args.get('minutes'))
+   print(year,month,date,hours,minutes)
+   result= predict_weather(year,month,date,hours,minutes)
+
+   return result
